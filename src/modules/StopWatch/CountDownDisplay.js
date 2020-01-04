a67b0e33c1eecf327c6b23a9340b8d5c0c347ea4
Xun Zhou <segentor@gmail.com>
2020-01-04 16:13:44 +0100

    updates

--- src/modules/StopWatch/CountDownDisplay.js
+++ src/modules/StopWatch/CountDownDisplay.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {secondsToTime} from "../../utils/TimeHelper";
 
@@ -9,45 +9,72 @@ const clearAllIntervals = () => {
     intervals = [];
 };
 
-const CountDownDisplay = ({counterInSeconds, resetOption = () => {}}) => {
-    const [seconds, setSeconds] = useState(0);
+class CountDownDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {seconds: 0};
+    };
 
-    const countDown = useCallback((value) => {
-        if (value === 0) {
+    reset = () => {
+        clearAllIntervals();
+        this.setState({seconds: 0});
+        window.alert.play()
+            .then(() => {
+                console.log('alert played');
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    countDown(counterInSeconds) {
+        if (counterInSeconds === 0) {
             return;
         }
 
         clearAllIntervals();
-        setSeconds(counterInSeconds);
+
+        this.setState({seconds: counterInSeconds});
         const intervalId = setInterval(() => {
             counterInSeconds--;
-            setSeconds(counterInSeconds);
+            this.setState({seconds: counterInSeconds});
             if (counterInSeconds === 0) {
-                clearAllIntervals();
-                window.alert.play()
-                    .then(() => {console.log('alert played');})
-                    .catch((error) => {console.log(error);});
-                resetOption();
+                this.reset();
+                this.props.resetOption();
             }
         }, 1000);
         intervals.push(intervalId);
-    }, [counterInSeconds, resetOption]);
+    }
 
-    useEffect(() => {
-        countDown(counterInSeconds);
-    }, [countDown, counterInSeconds]);
+    componentDidMount() {
+        this.props.onRef(this);
+    }
 
-    return (
-        <div className="timer-window">
-            <div className="time-display timer-display">
-                {secondsToTime(seconds)}
+    componentWillUnmount() {
+        this.props.onRef(undefined);
+    }
+
+    componentDidUpdate({counterInSeconds}, prevState, snapshot) {
+        if (this.props.counterInSeconds
+            && counterInSeconds !== this.props.counterInSeconds) {
+            this.countDown(3);
+        }
+    }
+
+    render() {
+        return (
+            <div className="timer-window">
+                <div className="time-display timer-display">
+                    {secondsToTime(this.state.seconds)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 CountDownDisplay.propTypes = {
     counterInSeconds: PropTypes.number,
+    onRef: PropTypes.func,
     resetOption: PropTypes.func,
 };
 
--- src/modules/StopWatch/index.js
+++ src/modules/StopWatch/index.js
@@ -1,72 +1,85 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import CountDownDisplay from "./CountDownDisplay";
 
-const StopWatch = ({toggleBgColor}) => {
-    const [selectedValue, setSelectedValue] = useState(0);
+class StopWatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {selectedValue: 0};
+    }
 
-    const handleClickOnOptionButton = (optionValue) => {
-        console.log('selected option(m): ' + selectedValue);
-        setSelectedValue(optionValue);
+    handleClickOnOptionButton = (optionValue) => {
+        this.setState({selectedValue: optionValue});
     };
 
-    const handleResetOption = () => {
-        setSelectedValue(0);
+    handleResetOption = () => {
+        this.setState({selectedValue: 0});
     };
 
-    const TimerOptionButton = ({optionValue, children = null}) => {
-        const isSelected = optionValue === selectedValue;
-        
-        return (
-            <button className={classNames("button timer-option", isSelected && "active")}
-                    disabled={isSelected}
-                    onClick={(event) => {
-                        handleClickOnOptionButton(optionValue);
-                    }}>
-                {`${optionValue < 10 ? "0" + optionValue : optionValue} m`}
-                {children}
-            </button>
-        );
+    handleOnClick = () => {
+        this.setState({selectedValue: 0});
+        this.child.reset();
     };
 
-    TimerOptionButton.propTypes = {
-        optionValue: PropTypes.number.isRequired,
-        children: PropTypes.object,
-    };
+    render() {
+        const {toggleBgColor} = this.props;
+        const TimerOptionButton = ({optionValue, children = null}) => {
+            const isSelected = optionValue === this.state.selectedValue;
 
-    return (
-        <div className={classNames("container")}>
-            <CountDownDisplay counterInSeconds={selectedValue * 60} resetOption={handleResetOption}/>
-            <div className="timer-options">
-                <button className="button timer-option" onClick={() => {
-                }}>
-                    {"00 m"}
-                    <span className="icon-spinner11"/>
+            return (
+                <button className={classNames("button timer-option", isSelected && "active")}
+                        disabled={isSelected}
+                        onClick={(event) => {
+                            this.handleClickOnOptionButton(optionValue);
+                        }}>
+                    {`${optionValue < 10 ? "0" + optionValue : optionValue} m`}
+                    {children}
                 </button>
-                <TimerOptionButton optionValue={3}>
-                    <span className="icon-grin"/>
-                </TimerOptionButton>
+            );
+        };
+
+        TimerOptionButton.propTypes = {
+            optionValue: PropTypes.number.isRequired,
+            children: PropTypes.object,
+        };
+
+        return (
+            <div className={classNames("container")}>
+                <CountDownDisplay
+                    onRef={ref => (this.child = ref)}
+                    counterInSeconds={this.state.selectedValue * 60}
+                    resetOption={this.handleResetOption}
+                />
+                <div className="timer-options">
+                    <button className="button timer-option" onClick={this.handleOnClick}>
+                        {"clear"}
+                        <span className="icon-spinner11"/>
+                    </button>
+                    <TimerOptionButton optionValue={3}>
+                        <span className="icon-grin"/>
+                    </TimerOptionButton>
+                </div>
+                <div className="timer-options">
+                    <TimerOptionButton optionValue={10}>
+                        <span className="icon-mug"/>
+                    </TimerOptionButton>
+                    <TimerOptionButton optionValue={12}>
+                        <span className="icon-local_pizza"/>
+                    </TimerOptionButton>
+                </div>
+                <div className="timer-options">
+                    <TimerOptionButton optionValue={25}>
+                        <span className="icon-radio-checked"/>
+                    </TimerOptionButton>
+                    <TimerOptionButton optionValue={45}>
+                        <span className="icon-bubbles4"/>
+                    </TimerOptionButton>
+                </div>
             </div>
-            <div className="timer-options">
-                <TimerOptionButton optionValue={10}>
-                    <span className="icon-mug"/>
-                </TimerOptionButton>
-                <TimerOptionButton optionValue={12}>
-                    <span className="icon-local_pizza"/>
-                </TimerOptionButton>
-            </div>
-            <div className="timer-options">
-                <TimerOptionButton optionValue={25}>
-                    <span className="icon-radio-checked"/>
-                </TimerOptionButton>
-                <TimerOptionButton optionValue={45}>
-                    <span className="icon-bubbles4"/>
-                </TimerOptionButton>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 StopWatch.propTypes = {
     toggleBgColor: PropTypes.func.isRequired,
